e7ffff4b89fa704e3095089776ec434b9f463b5c
Arpit <mish12arpit@gmail.com>
2024-06-25 09:09:04 +0530

    Signin/up

--- app/screens/auth/LoginScreen.tsx
+++ app/screens/auth/LoginScreen.tsx
@@ -1,40 +1,69 @@
-// app/screens/Auth/LoginScreen.tsx
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
 
 type Props = StackScreenProps<RootStackParamList, 'Login'>;
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
-  const [email, setEmail] = useState('');
+  const [mobile, setMobile] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
     // Handle login logic
-  };
+  }; 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title="Login" onPress={handleLogin} />
-      <Button
-        title="Go to Sign Up"
-        onPress={() => navigation.navigate('Signup')}
-      />
+      <Text style={styles.title}>Let's Sign In :)</Text>
+
+      <Text style={styles.label}>Mobile</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Eg: +91-85329802115"
+          value={mobile}
+          onChangeText={setMobile}
+          keyboardType="phone-pad"
+        />
+      </View>
+
+      <Text style={styles.label}>Password</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Eg: LoveMeLikeYouDo"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+      </View>
+
+      <TouchableOpacity style={styles.forgotPassword}>
+        <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.primaryButton} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+
+      <View style={styles.container1}>
+        <View style={styles.line}></View>
+        <Text style={styles.signInWith}>Sign In with</Text>
+        <View style={styles.line}></View>
+      </View>
+
+      <View style={styles.socialIconsContainer}>
+        <Icon name="twitter" size={30} color="#000000" style={styles.socialIcon} />
+        <Icon name="linkedin" size={30} color="#000000" style={styles.socialIcon} />
+        <Icon name="google" size={30} color="#000000" style={styles.socialIcon} />
+        <Icon name="facebook" size={30} color="#000000" style={styles.socialIcon} />
+      </View>
+
+      <TouchableOpacity style={styles.newUserSignUp} onPress={() => navigation.navigate('Signup')}>
+        <Text style={styles.newUserSignUpText}>New User? Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -45,17 +74,98 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
+    backgroundColor: '#F5F5F5',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#000000',
+    fontFamily: 'Poppins',
+  },
+  label: {
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000',
+    fontFamily: 'Poppins',
+  },
+  inputContainer: {
+    width: '100%',
+    marginBottom: 20,
   },
   input: {
     width: '100%',
-    padding: 10,
-    marginVertical: 5,
+    padding: 15,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: '#000000',
+    borderRadius: 8,
+    fontSize: 14,
+    color: '#000000',
+    fontFamily: 'Poppins',
+  },
+  forgotPassword: {
+    alignSelf: 'flex-end',
+    marginBottom: 30,
+  },
+  forgotPasswordText: {
+    fontSize: 14,
+    color: 'rgba(0, 0, 0, 0.5)',
+    fontFamily: 'Poppins',
+  },
+  primaryButton: {
+    backgroundColor: '#FFCB47',
+    paddingVertical: 15,
+    paddingHorizontal: 140,
+    borderRadius: 8,
+    marginTop: 20,
+    borderColor: '#000000',
+    borderWidth: 1.5,
+  },
+  buttonText: {
+    color: '#000000',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: 'Poppins',
+  },
+  container1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  line: {
+    flex: 1,
+    height: 2,
+    backgroundColor: '#000000',
+    marginHorizontal: 10,
+  },
+  signInWith: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000',
+    fontFamily: 'Poppins',
+  },
+  socialIconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%',
+    marginTop: 10,
+  },
+  socialIcon: {
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: '#FFFFFF',
+  },
+  newUserSignUp: {
+    marginTop: 20,
+  },
+  newUserSignUpText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000',
+    fontFamily: 'Poppins',
   },
 });
 
--- app/screens/auth/SignupScreen.tsx
+++ app/screens/auth/SignupScreen.tsx
@@ -1,47 +1,78 @@
-// app/screens/Auth/SignupScreen.tsx
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
 
 type Props = StackScreenProps<RootStackParamList, 'Signup'>;
 
 const SignupScreen: React.FC<Props> = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [officialEmail, setOfficialEmail] = useState('');
+  const [agreedToTerms, setAgreedToTerms] = useState(false);
 
   const handleSignup = () => {
-    // Handle signup logic
+    // Handle signup logic 
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Sign Up</Text>
+      <Text style={styles.title}>Let's Sign Up :)</Text>
+      <Text style={styles.label}>Full Name</Text>
+      <View style={styles.inputWithButton}>
       <TextInput
         style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
+        placeholder="Write your Name"
+        value={fullName}
+        onChangeText={setFullName}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title="Sign Up" onPress={handleSignup} />
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate('Login')}
-      />
+      </View>
+
+      <Text style={styles.label}>Mobile</Text>
+      <View style={styles.inputWithButton}>
+        <TextInput
+          style={styles.input}
+          placeholder="Write Mobile Number"
+          value={mobileNumber}
+          onChangeText={setMobileNumber}
+          keyboardType="phone-pad"
+        />
+        
+      </View>
+
+      <Text style={styles.label}>Official Email</Text>
+      <View style={styles.inputWithButton}>
+        <TextInput
+          style={styles.input}
+          placeholder="Write your Email"
+          value={officialEmail}
+          onChangeText={setOfficialEmail}
+          keyboardType="email-address"
+        />
+        
+      </View>
+
+      
+
+      <TouchableOpacity style={styles.primaryButton} onPress={handleSignup}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <View style={styles.signupWithContainer}>
+        <View style={styles.line}></View>
+        <Text style={styles.signupWithText}>Sign Up with</Text>
+        <View style={styles.line}></View>
+      </View>
+      <View style={styles.socialIconsContainer}>
+        <Icon name="twitter" size={30} color="#000000" style={styles.socialIcon} />
+        <Icon name="linkedin" size={30} color="#000000" style={styles.socialIcon} />
+        <Icon name="google" size={30} color="#000000" style={styles.socialIcon} />
+        <Icon name="facebook" size={30} color="#000000" style={styles.socialIcon} />
+      </View>
+
+     
+
     </View>
   );
 };
@@ -52,17 +83,106 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
+    backgroundColor: '#F5F5F5',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#000000',
   },
   input: {
-    width: '100%',
-    padding: 10,
-    marginVertical: 5,
+    flex: 1,
+    padding: 11,
+    marginBottom: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: '#000000',
+    borderRadius: 8,
+    fontSize: 16,
+    color: '#000000',
+  },
+  inputWithButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  primaryButton1: {
+    marginLeft: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#000000',
+    backgroundColor: '#FFFFFF',
+  },
+  primaryButton2: {
+    marginLeft: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#000000',
+    backgroundColor: '#FFFFFF',
+  },
+  buttonText1: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: 'rgba(0, 0, 0, 0.5)',
+  },
+  buttonText2: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: 'rgba(0, 0, 0, 0.5)',
+  },
+  label: {
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000',
+  },
+  
+  primaryButton: {
+    backgroundColor: '#FFCB47',
+    paddingVertical: 15,
+    paddingHorizontal: 140,
+    borderRadius: 8,
+    marginTop: 20,
+    borderColor: '#000000',
+    borderWidth: 1.5,
+  },
+  buttonText: {
+    color: '#000000',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  signupWithContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  line: {
+    flex: 1,
+    height: 2,
+    backgroundColor: '#000000',
+    marginHorizontal: 10,
+  },
+  signupWithText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000',
+  },
+  socialIconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%',
+    marginTop: 10,
+  },
+  socialIcon: {
+    padding: 10,
+    borderRadius: 50,
+    backgroundColor: '#FFFFFF',
   },
 });
 
